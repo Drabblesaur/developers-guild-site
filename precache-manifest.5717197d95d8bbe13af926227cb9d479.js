9c28eb884d8c7f163553c51d2dbc6ae98f7c243b
Carlos Alba <soradev4@gmail.com>
2019-10-04 20:45:47 -0700

    Updates

---
+++ precache-manifest.5717197d95d8bbe13af926227cb9d479.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39749e6ea49113083d2a755094a99766",
+    "url": "/developers-guild-site/index.html"
+  },
+  {
+    "revision": "5519066d1c3d635f3d1c",
+    "url": "/developers-guild-site/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "61173680c9ca2fe5b804",
+    "url": "/developers-guild-site/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5519066d1c3d635f3d1c",
+    "url": "/developers-guild-site/static/js/2.f2ca4ee6.chunk.js"
+  },
+  {
+    "revision": "61173680c9ca2fe5b804",
+    "url": "/developers-guild-site/static/js/main.93cd0804.chunk.js"
+  },
+  {
+    "revision": "98f4554981502a6a9953",
+    "url": "/developers-guild-site/static/js/runtime-main.5a9a14bd.js"
+  }
+]);
